323016a85ab7ba0270ac4af54eb77fd8a5a0483b
Vladyslava24 <vlada.lada.17@gmail.com>
2020-02-15 22:12:36 +0200

    Encapsulation_02

--- src/ua/kpi/tef/Main.java
+++ src/ua/kpi/tef/Main.java
@@ -4,9 +4,10 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         Point point = new Point();
-        point.x = 3;
-        point.y = 5;
-        int coordinateX = point.x;
-        int coordinateY = point.y;
+        point.setX(3);
+        point.setY(5);
+        int coordinateX = point.getX();
+        int coordinateY = point.getY();
+        System.out.println(coordinateX+" "+coordinateY);
     }
 }
--- src/ua/kpi/tef/Point.java
+++ src/ua/kpi/tef/Point.java
@@ -1,6 +1,22 @@
 package ua.kpi.tef;
 
 public class Point {
-    public int x;
-    public int y;
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
